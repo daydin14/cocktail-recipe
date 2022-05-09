90fb7f0d535df13856cb077412ae23ab00b6bc97
David Aydin <david.aydin6@gmail.com>
2022-05-09 12:10:56 -0400

    Base getdata function to get the data and push onto the screen. Currently does not work but the aim is to get this functioning by today.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert("JS is working");
+const url ="www.thecocktaildb.com/api/json/v1/1/random.php";
+
+const $form = $("form");
+const $drinkName = $("#drink");
+const $instructions = $("#instructions");
+const $image = $("#image");
+
+$form.on("submit", getData);
+
+function getData(event) {
+    event.preventDefault();
+
+    $.ajax(url).then(function(data) {
+        $drinkName.text(data.strDrink);
+        $instructions.text(data.strInstructions);
+        $image.attr("src", data.strDrinkThumb);
+        console.log(data);
+    })
+}
