b09aada00979064812bc9bc86ef53095b1681c80
David Aydin <david.aydin6@gmail.com>
2022-05-11 17:04:57 -0400

    Linked random button to nav bar. Edited some css to adjust the size of the image being displayed.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-// Top Menu Buttons:
-var menu = [
-  { text: "Home"},
-  { text: "Random Drink"},
-  { text: "Liquor Choice"},
-  { text: "Search Drink"},
-  { text: "All Drinks"},
-];
+// // Top Menu Buttons:
+// var menu = [
+//   { text: "Home"},
+//   { text: "Random Drink"},
+//   { text: "Liquor Choice"},
+//   { text: "Search Drink"},
+//   { text: "All Drinks"},
+// ];
 
-const topMenuEl = document.getElementById("menu-bar");
-topMenuEl.style.height = "100%";
-//topMenuEl.style.backgroundColor = '<img src="https://images.pexels.com/photos/3566120/pexels-photo-3566120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">'
-topMenuEl.classList.add("flex-around");
+// const topMenuEl = document.getElementById("menu-bar");
+// topMenuEl.style.height = "100%";
+// //topMenuEl.style.backgroundColor = '<img src="https://images.pexels.com/photos/3566120/pexels-photo-3566120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">'
+// topMenuEl.classList.add("flex-around");
 
-for (let i of menu) {
-  let bEl = document.createElement("button");
-  bEl.setAttribute("submit", menu);
-  bEl.textContent = i.text;
-  topMenuEl.append(bEl);
-}
+// for (let i of menu) {
+//   let bEl = document.createElement("button");
+//   bEl.setAttribute("submit", menu);
+//   bEl.textContent = i.text;
+//   topMenuEl.append(bEl);
+// }
 
 // Ajax random cocktail drink:
 const randomUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
@@ -27,11 +27,12 @@ const $drinkName = $("#drink");
 const $instructions = $("#instructions");
 const $image = $("#image");
 
-
 const $input = $(`input[type="text"]`);
-const $menuButton = $();
 
-$form.on("submit", getData);
+const $randomButton = $("#rand");
+// console.log($randomButton);
+
+$randomButton.on("click", getData);
 
 // Function to connect to api and get a random drink and display it once a form is submitted
 function getData(event) {
