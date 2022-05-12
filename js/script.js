7953432f5d197efc9e0960e3ab117f1711513ae0
David Aydin <david.aydin6@gmail.com>
2022-05-11 20:41:34 -0400

    Created a function to get all the ingredients and map them into a new array. Slightly updated the css.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,13 @@
-// // Top Menu Buttons:
-// var menu = [
-//   { text: "Home"},
-//   { text: "Random Drink"},
-//   { text: "Liquor Choice"},
-//   { text: "Search Drink"},
-//   { text: "All Drinks"},
-// ];
-
-// const topMenuEl = document.getElementById("menu-bar");
-// topMenuEl.style.height = "100%";
-// //topMenuEl.style.backgroundColor = '<img src="https://images.pexels.com/photos/3566120/pexels-photo-3566120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">'
-// topMenuEl.classList.add("flex-around");
-
-// for (let i of menu) {
-//   let bEl = document.createElement("button");
-//   bEl.setAttribute("submit", menu);
-//   bEl.textContent = i.text;
-//   topMenuEl.append(bEl);
-// }
-
 // Ajax random cocktail drink:
 const randomUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-const $form = $("form");
 const $drinkName = $("#drink");
 const $instructions = $("#instructions");
 const $image = $("#image");
+const $drinkIngredients = $("#ingredients");
 
 const $input = $(`input[type="text"]`);
-
 const $randomButton = $("#rand");
-// console.log($randomButton);
 
 $randomButton.on("click", getData);
 
@@ -43,11 +20,41 @@ function getData(event) {
       $drinkName.text(data.drinks[0].strDrink);
       $instructions.text(data.drinks[0].strInstructions);
       $image.attr("src", data.drinks[0].strDrinkThumb);
+
+      $drinkIngredients.text(getIngredients(data));
+      console.log($drinkIngredients)
     },
     function (error) {
       console.log("Something is wrong");
       console.log(error);
     }
-  );
+  )
 }
 
+function getIngredients(data) {
+  let ingredients = [];
+  ingredients.push(data.drinks[0].strIngredient1);
+  ingredients.push(data.drinks[0].strIngredient2);
+  ingredients.push(data.drinks[0].strIngredient3);
+  ingredients.push(data.drinks[0].strIngredient4);
+  ingredients.push(data.drinks[0].strIngredient5);
+  ingredients.push(data.drinks[0].strIngredient6);
+  ingredients.push(data.drinks[0].strIngredient7);
+  ingredients.push(data.drinks[0].strIngredient8);
+  ingredients.push(data.drinks[0].strIngredient9);
+  ingredients.push(data.drinks[0].strIngredient10);
+  ingredients.push(data.drinks[0].strIngredient11);
+  ingredients.push(data.drinks[0].strIngredient12);
+  ingredients.push(data.drinks[0].strIngredient13);
+  ingredients.push(data.drinks[0].strIngredient14);
+  ingredients.push(data.drinks[0].strIngredient15);
+
+  console.log(ingredients);
+
+  let filteredIngredients = ingredients.map(function(ingredient) {
+    if (ingredient != null) {
+      return ingredient;
+    }
+  });
+  return filteredIngredients;
+}
